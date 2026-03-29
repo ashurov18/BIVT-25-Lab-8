8da63b3a301bef31fcb17e606ead7bd4198695e6
ashurov18 <ashurovburhoniddin08@gmail.com>
2026-03-29 22:53:39 +0300

    Update Task2.cs

--- Lab8/White/Task2.cs
+++ Lab8/White/Task2.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Linq;
-
 namespace Lab8.White
 {
     public class Task2
@@ -9,36 +6,131 @@ namespace Lab8.White
         {
             private string _name;
             private string _surname;
-            private double _jump1;
-            private double _jump2;
+            private double _firstjump;
+            private double _secondjump;
 
-            private const int _standard = 3;
 
-            public static int Standard => _standard;
+            private static readonly double _standard;
+            
 
-            public bool IsPassed => _jump1 >= _standard || _jump2 >= _standard;
 
-            static Participant()
-            {
-            }
-
-            public Participant(string name, string surname, double jump1, double jump2)
-            {
-                _name = name;
-                _surname = surname;
-                _jump1 = jump1;
-                _jump2 = jump2;
-            }
 
             public string Name => _name;
             public string Surname => _surname;
-            public double Jump1 => _jump1;
-            public double Jump2 => _jump2;
+            public double FirstJump => _firstjump;
+            public double SecondJump => _secondjump;
+            
 
+            public double BestJump
+            {
+                get
+                {
+                    if (_firstjump == 0 && _secondjump == 0)
+                        return 0;
+                    else if (_firstjump != 0 && _secondjump == 0)
+                        return FirstJump;
+                    else if (_secondjump != 0 && _firstjump == 0)
+                        return SecondJump;
+                    else
+                        return Math.Max(_firstjump, _secondjump);
+
+                }
+            }
+
+            public bool IsPassed
+            {
+                get
+                {
+                    return BestJump>=_standard; 
+                }
+            }
+
+            static Participant()
+            {
+                _standard = 3.0;
+            }
+            public Participant(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _firstjump=0;
+                _secondjump=0;
+
+            }
+            public Participant(string name, string surname, double firstjump, double secondjump)
+            {
+                _name = name;
+                _surname = surname;
+                _firstjump = firstjump;
+                _secondjump = secondjump;
+            }
+
+
+
+        
             public static Participant[] GetPassed(Participant[] participants)
             {
-                if (participants == null) return new Participant[0];
-                return participants.Where(p => p.IsPassed).ToArray();
+                if (participants == null || participants.Length == 0)
+                    return new Participant[0];
+                int count = 0;
+                for (int i = 0; i < participants.Length; i++)
+                {
+                    if (participants[i].IsPassed)      
+                        count++;
+                }
+                Participant[] result = new Participant[count];
+                
+                int k = 0;
+                for (int i = 0; i < participants.Length; i++)
+                {
+                    if (participants[i].IsPassed)
+                    {
+                        result[k++]=participants[i];
+                    }
+                }
+                return result;
+
+            }
+
+
+
+
+
+            public void Jump(double result)
+            {
+                if (_firstjump == 0)
+                {
+                    _firstjump = result;
+                }
+                else if (_secondjump == 0)
+                {
+                    _secondjump = result;
+                }
+
+            }
+            public static void Sort(Participant[] array)
+            {
+                if (array == null || array.Length == 0)
+                    return;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 1; j < array.Length; j++)
+                    {
+                        if (array[j - 1].BestJump < array[j].BestJump)
+                        {
+                            (array[j - 1], array[j]) = (array[j], array[j - 1]);
+
+                        }
+                    }
+                }
+            }
+            public void Print()
+            {
+                Console.WriteLine(_name);
+                Console.WriteLine(_surname);
+                Console.WriteLine(_firstjump);
+                Console.WriteLine(_secondjump);
+                Console.WriteLine(BestJump);
             }
         }
     }
